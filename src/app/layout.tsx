5cfbc58b04b7c238ee96fedfa0bddf41812c4e36
mrnzdev <mrnzdev@proton.me>
2024-08-07 18:17:30 -0300

    chore: Import @vercel/speed-insights/next for performance monitoring

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from 'next';
 import { Montserrat } from 'next/font/google';
 import { Analytics } from '@vercel/analytics/react';
+import { SpeedInsights } from "@vercel/speed-insights/next"
+
 import './globals.css';
 
 const montserrat = Montserrat({ subsets: ['latin'] });
@@ -25,6 +27,7 @@ const RootLayout = ({
 			>
 				{children}
 				<Analytics />
+				<SpeedInsights />
 			</body>
 		</html>
 	);

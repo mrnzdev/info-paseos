b1764e372ff9201a661ee670afdfbfa4aaa523d7
mrnzdev <mrnzdev@proton.me>
2024-08-08 16:32:18 -0300

    chore: Update HeaderSection to include responsive logo size and link

--- src/components/HeaderSection.tsx
+++ src/components/HeaderSection.tsx
@@ -5,12 +5,15 @@ export default function HeaderSection() {
 	return (
 		<section className='h-dvh flex flex-col justify-between py-12'>
 			<header>
-				<Image
-					src={'/logo.svg'}
-					alt='Logo de Info Paseos'
-					width={175}
-					height={175}
-				/>
+				<Link href={'/'}>
+					<Image
+						src={'/logo.svg'}
+						alt='Logo de Info Paseos'
+						width={175}
+						height={175}
+						className='w-32 lg:w-48'
+					/>
+				</Link>
 			</header>
 			<div className='flex flex-col items-center gap-6'>
 				<h1 className='w-[23ch] text-[calc(2.5vw+1rem)] text-center font-medium'>
@@ -28,7 +31,7 @@ export default function HeaderSection() {
 			<div className='flex justify-center md:justify-end'>
 				<Link
 					href='mailto:contacto@infopaseos.com'
-					className='bg-[#F7572B] px-12 py-6 rounded-[50px] font-bold text-xl'
+					className='bg-[#F7572B] md:px-12 md:py-6 px-10 py-4 rounded-[50px] font-bold text-md md:text-xl'
 				>
 					Quiero publicar un paseo
 				</Link>

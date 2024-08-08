4fb6b19310b7127d83307583bb9ab2793f59ab0d
mrnzdev <mrnzdev@proton.me>
2024-08-08 16:31:39 -0300

    chore: Sort imports

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,6 @@ import type { Metadata } from 'next';
 import { Montserrat } from 'next/font/google';
 import { Analytics } from '@vercel/analytics/react';
 import { SpeedInsights } from "@vercel/speed-insights/next"
-
 import './globals.css';
 
 const montserrat = Montserrat({ subsets: ['latin'] });
